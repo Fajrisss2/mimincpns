007badcf3fa1e0cc671b959078a08967129d272d
Anisya <mba_anisya@outlook.com>
2024-08-20 14:02:24 +0700

    init commit

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 { name: 'Komisi Pemilihan Umum(KPU)', url: 'https://chat.whatsapp.com/EMMX5HzqpnH3hItIBdeYmT' },
                 { name: 'Perpustakaan nasional', url: 'https://chat.whatsapp.com/BQdv3Op5EwvB31VUzyprXD' },
                 { name: 'Kemendagri', url: 'https://chat.whatsapp.com/K7TRgs3WmQa1qDVINVJ9ps' },
+                { name: 'Kemensos', url: 'https://chat.whatsapp.com/CrKdMhKYfUWH2lv3ek7DNa' },
                 { name: 'Bps', url: 'https://chat.whatsapp.com/Cd7jUaDOwWuGMo2QIIqQEC' },
                 { name: 'Kementerian Pertahanan', url: 'https://chat.whatsapp.com/FpCIJ73x3O2KAiWLm5mYwO' },
                 { name: 'Kementerian Perhubungan', url: 'https://chat.whatsapp.com/GB8BIhFp870DPX4peVUECA' },
